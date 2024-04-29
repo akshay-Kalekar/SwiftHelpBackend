fa311ce8a795fe953394da1cd0fec4f76f01c186
Akshay <akalekar2003@gmail.com>
2024-04-29 23:45:57 +0530

    netlify

--- server.js
+++ server.js
@@ -5,21 +5,11 @@ const user  = require("./routes/User");
 const authRoute  = require("./routes/authroute");
 const { auth } = require('express-openid-connect');
 
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: 'a long, randomly-generated string stored in env',
-  baseURL: 'http://localhost:3000',
-  clientID: '3QeSotK0fDGAjI3Sux3aYuisD0klpjGV',
-  issuerBaseURL: 'https://dev-56eofp2t8ujgwtap.us.auth0.com'
-};
-
 
 var cors = require('cors')
 require('dotenv').config()  
 
 const app  = express();
-app.use(auth(config));
 
 app.use(bodyParser.urlencoded({ extended: false }))
 
@@ -52,3 +42,4 @@ app.listen(PORT,async ()=>{
       }
     console.log("server running on ", PORT)
 })
+module.exports = app;
